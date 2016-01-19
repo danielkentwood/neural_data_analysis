b95721db9ac5cb49840b43603845e4e80198d8cf
daniel wood <danielkentwood@gmail.com>
2016-01-19 17:58:24 -0600

    update buildSpikeTrain
    can handle empty matrix for spikeTimes variable

--- buildSpikeTrain.m
+++ buildSpikeTrain.m
@@ -1,7 +1,7 @@
 % buildSpikeTrain.m
 %
-% Inputs    spikeTimes: a vector or a cell array of vectors containing 
-%               spike times. 
+% Inputs    spikeTimes: a vector or a cell array of vectors containing
+%               spike times.
 %           zeroTime: the time of the event to which you wish to lock.
 %           startTime: how much time before the zeroTime do you wish to
 %               include?
@@ -16,32 +16,41 @@
 
 function Sm = buildSpikeTrain(spikeTimes,zeroTime,startTime,endTime,dt)
 
+nospikes=0;
 if iscell(spikeTimes)
     numtrials=length(spikeTimes);
 elseif isvector(spikeTimes)
     spikeTimes = {spikeTimes};
     numtrials=1;
+elseif isempty(spikeTimes)
+    nospikes = 1;
 elseif ismatrix(spikeTimes)
     error('spikeTimes input needs to be either a vector or a cell array of vectors, not a matrix')
 end
 
-for tr=1:numtrials
-    
-    spTs = spikeTimes{tr};
-    
-    spTs=spTs-double(zeroTime);
-    spTs=spTs(spTs>startTime & spTs<endTime);
-    spTs=spTs+abs(startTime);
-    
-    T = endTime-startTime;
-    if startTime<=0, T=T+1; end
-    
-    S = sparse(zeros(1,ceil(T/dt)));
-    for j=1:length(spTs)
-        S(ceil((spTs(j))/dt)) = S(ceil(spTs(j)/dt))+1;
-    end
-    
-    Sm(tr,:)=S;
-end
+T = endTime-startTime;
+S = sparse(zeros(1,ceil(T/dt)));
 
-Sm=full(Sm);
+if nospikes
+    Sm=full(S);
+else
+    for tr=1:numtrials
+        
+        spTs = spikeTimes{tr};
+        
+        spTs=spTs-double(zeroTime);
+        spTs=spTs(spTs>startTime & spTs<endTime);
+        spTs=spTs+abs(startTime);
+        
+        
+        if startTime<=0, T=T+1; end
+        
+        
+        for j=1:length(spTs)
+            S(ceil((spTs(j))/dt)) = S(ceil(spTs(j)/dt))+1;
+        end
+        
+        Sm(tr,:)=S;
+    end
+    Sm=full(Sm);
+end
